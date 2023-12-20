5a3e9cd4298d1d22691cd04be461f14b0179ef19
Jeongseunghun <seunghun311@naver.com>
2023-12-20 19:05:42 +0900

    배열의 내장함수

--- 배열의 내장 함수_custom 함수.js
+++ 배열의 내장 함수_custom 함수.js
@@ -1,219 +1,211 @@
-// // 1. filter > customFilter 직접 구현
-// function customFilter(arr, callback) {
-//     const res = [];
+// 1. filter > customFilter 직접 구현
+function customFilter(arr, callback) {
+    const filterres = [];
 
-//     for (let i = 0 ; i < arr.length ; i++){
-//         if(callback(arr[i],i,arr)){
-//             res.push(arr[i]);
-//         }
-//     }
+    for (let i = 0 ; i < arr.length ; i++){
+        if(callback(arr[i],i,arr)){
+            filterres.push(arr[i]);
+        }
+    }
 
-//     return res;
-// }
+    return filterres;
+}
 
-// //콜백함수 예제
-// function overthree(str){
-//     return str.length > 3;
-// }
+//콜백함수 예제
+function overthree(str){
+    return str.length > 3;
+}
 
-// const words = ['hi','my','name','is','jeong'];
+const filterwords = ['hi','my','name','is','jeong'];
 
-// const longwords = customFilter(words,overthree);
+const longwords = customFilter(filterwords,overthree);
 
-// console.log(longwords);
+console.log(longwords);
 
-// // 1-1. filter 함수 사용
+// 1-1. filter 함수 사용
 
-// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+const filterres2 = filterwords.filter((word) => word.length > 3);
 
-// const res = words.filter((word) => word.length > 6);
+console.log(filterres2);
 
-// console.log(res);
 
+// 2. every > customEvery 직접 구현
+function customEvery(arr, callback){
+    for(let i = 0 ; i < arr.length; i++){
+        if(!callback(arr[i],i,arr)){
+            return false;
+        }
+    }
+    return true;
+}
 
-// // 2. every > customEvery 직접 구현
-// function customEvery(arr, callback){
-//     for(let i = 0 ; i < arr.length; i++){
-//         if(!callback(arr[i],i,arr)){
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+//콜백함수 예제
+function isEven(num) {
+    return num % 2 === 0;
+}
 
-// //콜백함수 예제
-// function isEven(num) {
-//     return num % 2 === 0;
-// }
+const everynum = [2,4,6,8,10];
 
-// const nums = [2,4,6,8,10];
+const alleven = customEvery(everynum,isEven);
 
-// const alleven = customEvery(nums,isEven);
+console.log(alleven)
 
-// console.log(alleven)
+// 2-1. every 함수 사용
+const isBelowThreshold = (currentVal) => currentVal < 40;
 
-// // 2-1. every 함수 사용
-// const isBelowThreshold = (currentVal) => currentVal < 40;
 
-// const arr = [1,3,10,20,30,4,23];
+console.log(everynum.every(isBelowThreshold));
 
-// console.log(arr.every(isBelowThreshold));
 
+// 3. some > customSome 직접 구현
+function customSome(arr,callback){
 
-// // 3. some > customSome 직접 구현
-// function customSome(arr,callback){
+    for(let i = 0; i < arr.length ; i++){
+        if(callback(arr[i],i,arr)){
+            return true;
+        }
 
-//     for(let i = 0; i < arr.length ; i++){
-//         if(callback(arr[i],i,arr)){
-//             return true;
-//         }
+    }
+    return false;
+}
 
-//     }
-//     return false;
-// }
+//콜백함수 예제
+function isEven(num){
+    return num % 2 === 0;
+}
 
-// //콜백함수 예제
-// function isEven(num){
-//     return num % 2 === 0;
-// }
+const somenum = [1,3,4,5,7,9];
 
-// const nums = [1,3,4,5,7,9];
+const hasEven = customSome(somenum,isEven)
 
-// const hasEven = customSome(nums,isEven)
+console.log(hasEven);
 
-// console.log(hasEven);
+// 3-1. some 함수 사용
 
-// // 3-1. some 함수 사용
+const even = (element) => element % 2 === 0;
 
-// const arr = [1,2,3,4,5];
+console.log(somenum.some(even));
 
-// const even = (element) => element % 2 === 0;
 
-// console.log(arr.some(even));
+// 4. forEach > customForEach 직접 구현
 
+function customForEach(arr,callback){
+    for(let i = 0; i < arr.length ; i++){
+        callback(arr[i],i,arr);
+    }
+}
 
-// // 4. forEach > customForEach 직접 구현
+//콜백함수 예제
+function logItem(item){
+    console.log(`${item}`);
 
-// function customForEach(arr,callback){
-//     for(let i = 0; i < arr.length ; i++){
-//         callback(arr[i],i,arr);
-//     }
-// }
+}
 
-// //콜백함수 예제
-// function logItem(item){
-//     console.log(`${item}`);
+const forEachstr = ['apple','banana','orange']
 
-// }
+customForEach(forEachstr,logItem);
 
-// const fruit = ['apple','banana','orange']
+// 4-1. forEach 함수 사용
 
-// customForEach(fruit,logItem);
+forEachstr.forEach((element) => console.log(element));
 
-// // 4-1. forEach 함수 사용
-// const arr = ['a','b','c']
 
-// arr.forEach((element) => console.log(element));
+// 5. map > customMap 직접 구현
+function customMap(arr,callback) {
+    const res = [];
 
+    for(let i = 0; i < arr.length; i++){
+        res.push(callback(arr[i],i,arr));
+    }
 
-// // 5. map > customMap 직접 구현
-// function customMap(arr,callback) {
-//     const res = [];
+    return res;
+}
 
-//     for(let i = 0; i < arr.length; i++){
-//         res.push(callback(arr[i],i,arr));
-//     }
+//콜백함수 예제
+function doubleNumber(num){
+    return num * 2;
+}
 
-//     return res;
-// }
+const mapnums = [1,2,3,4,5];
 
-// //콜백함수 예제
-// function doubleNumber(num){
-//     return num * 2;
-// }
+const doubleNumbers = customMap(mapnums,doubleNumber);
 
-// const nums = [1,2,3,4,5];
+console.log(doubleNumbers);
 
-// const doubleNumbers = customMap(nums,doubleNumber);
+// 5-1. map 함수 사용
 
-// console.log(doubleNumbers);
+const map1 = mapnums.map((x) => x * 2);
 
-// // 5-1. map 함수 사용
-// const nums = [1,2,3,4,5];
+console.log(map1);
 
-// const map1 = nums.map((x) => x * 2);
 
-// console.log(map1);
+// 6. reduce > customReduce 직접 구현
+function customRdeuce(arr,callback,initialValue) {
 
+    let accmulator = initialValue !== undefined ? initialValue: arr[0];
 
-// // 6. reduce > customReduce 직접 구현
-// function customRdeuce(arr,callback,initialValue) {
+    const startIndex = initialValue !== undefined ? 0 : 1;
 
-//     let accmulator = initialValue !== undefined ? initialValue: arr[0];
+    for (let i = startIndex; i < arr.length; i++) {
+        accmulator = callback(accmulator, arr[i], i, arr);
+    }
 
-//     const startIndex = initialValue !== undefined ? 0 : 1;
+    return accmulator;
+}
 
-//     for (let i = startIndex; i < arr.length; i++) {
-//         accmulator = callback(accmulator, arr[i], i, arr);
-//     }
+//콜백함수 예제
+function sum(accmulator, currentElement) {
+    return accmulator + currentElement;
+}
 
-//     return accmulator;
-// }
+const reducenums = [1,2,3,4,5];
 
-// //콜백함수 예제
-// function sum(accmulator, currentElement) {
-//     return accmulator + currentElement;
-// }
+const totalsum = customRdeuce(reducenums,sum,0);
 
-// const nums = [1,2,3,4,5];
+console.log(totalsum);
 
-// const totalsum = customRdeuce(nums,sum,0);
+// 6-1. reduce 함수 사용
 
-// console.log(totalsum);
+const initialVal = 0;
+const sumWithInitial = reducenums.reduce(
+    (accumulator, currentval) => accumulator + currentval, initialVal);
 
-// // 6-1. reduce 함수 사용
-// const arr = [1,2,3,4,5];
+console.log(sumWithInitial)
 
-// const initialVal = 0;
-// const sumWithInitial = arr.reduce(
-//     (accumulator, currentval) => accumulator + currentval, initialVal);
 
-// console.log(sumWithInitial)
+// 7. flat > customFlat 직접 구현
+function customFlat(arr,depth = 1) {
+    if(depth <= 0){
+        return arr.slice();
+    }
 
+    function flattenArray(array, currentDepth) {
+        return array.reduce((acc,val) => {
+            if(Array.isArray(val) && currentDepth < depth) {
+                return acc.concat(flattenArray(val,currentDepth+1));
+            }
+            else {
+                return acc.concat(val);
+            }
+        },[]);
+    }
 
-// // 7. flat > customFlat 직접 구현
-// function customFlat(arr,depth = 1) {
-//     if(depth <= 0){
-//         return arr.slice();
-//     }
+    return flattenArray(arr,1);
+}
 
-//     function flattenArray(array, currentDepth) {
-//         return array.reduce((acc,val) => {
-//             if(Array.isArray(val) && currentDepth < depth) {
-//                 return acc.concat(flattenArray(val,currentDepth+1));
-//             }
-//             else {
-//                 return acc.concat(val);
-//             }
-//         },[]);
-//     }
+const nestedArr = [1,[2,[3,4,[5,6]]]];
 
-//     return flattenArray(arr,1);
-// }
+const flatArr = customFlat(nestedArr,2);
 
-// const nestedArr = [1,[2,[3,4,[5,6]]]];
+console.log(flatArr);
 
-// const flatArr = customFlat(nestedArr,2);
+// 7-1. flat 함수 사용
+const flatarr1 = [0, 1, 2, [3, 4]];
 
-// console.log(flatArr);
+console.log(flatarr1.flat());
 
-// // 7-1. flat 함수 사용
-// const arr1 = [0, 1, 2, [3, 4]];
+const flatarr2 = [0, 1, [2, [3, [4, 5]]]];
 
-// console.log(arr1.flat());
-
-// const arr2 = [0, 1, [2, [3, [4, 5]]]];
-
-// console.log(arr2.flat());
-// console.log(arr2.flat(2));
-// console.log(arr2.flat(Infinity));
+console.log(flatarr2.flat());
+console.log(flatarr2.flat(2));
+console.log(flatarr2.flat(Infinity));
--- 배열의 내장 함수_실제 데이터 다루기.js
+++ 배열의 내장 함수_실제 데이터 다루기.js
@@ -1,7 +1,90 @@
-// 영화의 id, 제목, 년도, 평점, 장르 데이터를 갖고있는 리스트를 생성한다.
+// 1. 영화의 id, 제목, 년도, 평점, 장르 데이터를 갖고있는 리스트를 생성한다.
+// 모듈 불러오기
+const movieData = require('./data.js');
+
+// customMap 함수 사용
+function customMap(arr, callback) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(callback(arr[i], i, arr));
+    }
+    return result;
+  }
 
 
-// 평점을 input으로 받아 해당 평점 이상의 평점을 갖는 리스트를 반환한다.
+const movieList1 = customMap(movieData, movie => ({
+    id: movie.id,
+    title: movie.title,
+    year: movie.year,
+    rating: movie.rating,
+    genres: movie.genres
+  }));
+  
+console.log(movieList1);
 
 
-// 갖고있는 영화리스트들의 모든 장르를 추출해 중복되지 않은 장르 리스트를 생성한다.
+// lodash 라이브러리 사용
+const _ = require('lodash');
+
+const movieList2 = _.map(movieData, movie => _.pick(movie, ['id', 'title', 'year', 'rating', 'genres']));
+
+console.log(movieList2);
+
+
+// 2. 평점을 input으로 받아 해당 평점 이상의 평점을 갖는 리스트를 반환한다.
+// 모듈 불러오기
+// const movieData = require('./data.js');
+
+// customFilter 함수 사용
+function customFilter(arr, callback) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i], i, arr)) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+
+const inputRating = 8.0;
+
+const ratingList = customFilter(movieData, movie => movie.rating >= inputRating);
+
+console.log(ratingList);
+
+// lodash 라이브러리 사용
+// const _ = require('lodash');
+
+const ratingList2 = _.filter(movieData, movie => movie.rating >= inputRating);
+
+// 결과 출력
+console.log(ratingList2);
+  
+
+// 3. 갖고 있는 영화리스트들의 모든 장르를 추출해 중복되지 않은 장르 리스트를 생성한다.
+// 모듈 불러오기
+// const movieData = require('./data.js');
+
+// customMap 함수 사용
+function customMap2(arr, callback) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(callback(arr[i], i, arr));
+    }
+    return result;
+  }
+
+// 갖고 있는 영화 리스트들의 모든 장르 추출
+const allGenres = customMap2(movieData, movie => movie.genres)
+  .reduce((acc, genres) => acc.concat(genres), []); // 장르들을 합쳐 1차원 배열로 변환
+
+const uniqueGenres = [...new Set(allGenres)];
+
+console.log(uniqueGenres);
+
+// lodash 라이브러리 사용
+// const _ = require('lodash');
+
+const uniqueGenres2 = _.uniq(_.flatMap(movieData, 'genres'));
+
+console.log(uniqueGenres2);

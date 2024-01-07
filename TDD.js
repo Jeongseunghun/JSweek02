5c19e72316672d738c4575e602ecdd67eb727ced
Jeongseunghun <seunghun311@naver.com>
2024-01-07 18:07:50 +0900

    isEqual 수정

--- TDD.js
+++ TDD.js
@@ -1,48 +1,44 @@
-//assertion
-//1. assert(exception) 함수 작성
-function assert(exception){
-    if(!exception) {
-        throw new Error("error입니다.");
-    }
-    else{
-        return true;
-    }
-}
+// //assertion
+// //1. assert(exception) 함수 작성
+// function assert(exception){
+//     if(exception) {
+//         return true;
+//     }
+//     throw new Error("error입니다.");
+// }
 
-//예제
-let val = 10;
-assert(val === 20);
+// //예제
+// let val = 10;
+// assert(val === 20);
 
 
-//2. expect(value, equal) 함수 작성
-function expect(value,equal){
-    assert(value === equal);
-}
+// //2. expect(value, equal) 함수 작성
+// function expect(value,equal){
+//     assert(value === equal);
+// }
 
-//예제
-let res = (a,b) => a+b;
-expect(res(2,4),6);
+// //예제
+// let res = (a,b) => a+b;
+// expect(res(2,4),6);
 
-//예외발생 예제
-res = (a,b) => a * b;
-expect(res(3,5),12);
+// //예외발생 예제
+// res = (a,b) => a * b;
+// expect(res(3,5),12);
 
 //3. 주어진 결과가 값이 아닌 객체 또는 배열인 경우에도 같은 동작을 하도록
 
 function assert(exception){
     if(!exception) {
-        throw new Error("error입니다.");
+         throw new Error("error입니다.");
     }
-    else{
-        return true;
-    }
+    return true;
 }
 
 function isEqual(obj1,obj2) {
     if (obj1 === obj2) {
         return true;
     }
-    if (typeof obj1 !== 'object' || obj2 === null || typeof obj2 !== 'object' || obj2 === null){
+    if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null){
         return false;
     }
 
@@ -66,6 +62,6 @@ function expect(value,equal){
 }
 
 //예제
-let obj1 = { key: 'value' };
-let obj2 = { key: 'value' };
+let obj1 = { key: {} };
+let obj2 = { key: undefined };
 expect(obj1,obj2)
